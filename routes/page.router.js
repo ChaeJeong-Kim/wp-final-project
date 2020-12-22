7c40e7b7c267e0967825b42d6f02452fdd4a35ef
ChaeJeong-Kim <cowjd0729@naver.com>
2020-12-22 23:38:55 +0900

    complete

--- routes/page.router.js
+++ routes/page.router.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get('/', (req,res) => {
     res.render('index.html', {
-        title: 'Final Project',
+        title: 'ChaeJ-Blog',
         username: '김채정'
     });
 })
